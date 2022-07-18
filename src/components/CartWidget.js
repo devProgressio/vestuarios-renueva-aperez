82b2dfc74a6da57b5793e11d9505890e347fb635
devProgressio <dev.progressio@gmail.com>
2022-07-18 19:44:54 -0400

    navbar

---
+++ src/components/CartWidget.js
@@ -0,0 +1,10 @@
+import { Button } from 'primereact/button';
+
+const CartWidget = (props) => {
+
+    return (
+        <Button type="button" icon="pi pi-shopping-cart" className="p-button-warning" badge="8" badgeClassName="p-badge-danger" />
+    ); 
+}
+
+export default CartWidget;
