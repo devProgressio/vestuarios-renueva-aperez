c14a25401d515c85d433ae78c54c8c24788dc9a7
devProgressio <dev.progressio@gmail.com>
2022-09-02 01:43:20 -0400

    mejora diseño y agrega talla

--- src/components/item-detail.js
+++ src/components/item-detail.js
@@ -8,6 +8,9 @@ import { Button } from "primereact/button";
 import { Link } from "react-router-dom";
 import { Toast } from 'primereact/toast';
 import { CART_CONTEXT } from "../context/cart-context";
+import { formatNumber } from "../utils/format";
+import { Dropdown } from 'primereact/dropdown';
+import { SIZES } from "../utils/constants";
 
 const ItemDetail = ({ product }) => {
 
@@ -15,24 +18,23 @@ const ItemDetail = ({ product }) => {
     const { name, description, price, img, hashtags, stock } = product;
     const [val, setVal] = useState(null);
     const [terminar, setTerminar] = useState(false);
-    // const [message, setMessage] = useState(false);
-
+    const [size, setSize] = useState('');
+    const [quantity, setQuantity] = useState(1);
     const toastBR = useRef(null);
 
-    /*     const showBottomRight = (mens) => {
-            toastBR.current.show({severity:'success', summary: 'Se agregó producto al carro', detail:`${mens}`, life: 3000});
-        } */
+    const onAddQuantity = (quantity) => {
+        setQuantity(quantity);
+    }
 
-    const onAdd = (quantity) => {
+    const onAddCarro = (quantity, size) => {
         console.log('onAdd: ', quantity);
-        //setMessage(`Se agregó la cantidad de ${quantity} del producto ${name} a su carro de compras`);
-        addProduct(product, quantity);
+        addProduct(product, quantity, size);
         setTerminar(true);
     }
 
     const rating = (
         <div className="field grid">
-            <div className="flex-none" style={{ width: 150 }}>
+            <div className="flex-none pl-2" style={{ width: 150 }}>
                 <Rating value={val} cancel={false} onChange={(e) => setVal(e.value)} />
             </div>
             <div className="flex-none" style={{ width: 120 }}>
@@ -67,22 +69,30 @@ const ItemDetail = ({ product }) => {
                         <div className="col-8">
                             <div className="grid">
                                 <div className="col-12">
-                                    <h1>{name}</h1>
+                                    <span className="text-5xl font-bold text-teal-200">{name}</span>
+                                    <hr className="opacity-40 mb-4" />
                                     {rating}
-                                    <hr className="opacity-40" />
                                     <div className="font-light"> {description}</div>
-                                    <br />
-                                    <div className="font-bold text-xl">CLP ${price}</div>
+                                    <hr className="opacity-40 mb-4" />
+                                    <div className="font-bold text-3xl text-teal-200">$ {formatNumber(price)}</div>
+                                    <span className="text-xs opacity-70">Hasta agotar stock</span>
                                 </div>
                                 <div className="col-12">
-                                    <ItemCount stock={stock} initial={1} onAdd={onAdd} />
-                                    <div className="text-xs font-italic">Quedan aproximadamente {stock} en stock</div>
-                                    {/* {message && <p>{message}</p>} */}
-                                    <br></br>
+                                    <span className="col p-5">
+                                        <Dropdown value={size} options={SIZES} onChange={(e) => setSize(e.value)} placeholder="Talla" optionLabel="code" optionValue="code"
+                                            style={{ width: '40em' }} inputId="size"/>
+                                    </span>
+                                    <span className="mr-2">
+                                        <ItemCount stock={stock} initial={1} onAdd={onAddQuantity} size={size} />
+                                    </span>
+                                    
+                                    <div className="text-xs font-italic opacity-70 mb-6">Quedan aproximadamente {stock} en stock</div>
+                                </div>
+                                <div className="col">
                                     <Link to={`/cart`}>
                                         <Button label="Ir al Carrito" className="p-button-outlined p-button-rounded p-button-sm mr-2"></Button>
                                     </Link>
-                                    { terminar ? botonTerminarCompra : null }
+                                    {terminar ? botonTerminarCompra : null}
                                 </div>
                             </div>
                         </div>
